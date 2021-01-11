482689a496b4ae714d2c323b360c5feb95c6cab7
Marco Guidi <mark.88g@gmail.com>
2021-01-11 18:46:58 +0100

    + scaricate le categorie e generato il menu a tendina

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 $('document').ready( function() {
-    
+
+    // get all the category
+    $.ajax({
+        url: 'https://api.chucknorris.io/jokes/categories',
+        method: 'GET',
+        success: function(data) {
+            data.forEach( (current_category) => {
+                console.log(current_category);
+                $('#jokes-category').append('<option value=' + current_category + '>' + current_category + '</option>')
+            });
+
+            // $('#jokes-category').append('<option value="2">2</option>')
+        }
+    });
+
+    // get the change of the select
+    $('#jokes-category').change(function() {
+        console.log("preso");
+    })
 });
